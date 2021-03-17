4a1aa25c848b23b7a4156d3ab73d2c0c82b55b2e
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-03-17 10:43:15 +0530

    Rendered products through JSX

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            name: "Programmer's TShirt 1",
+            imagePath: "/images/tshirt1.jpeg",
+            price: 560,
+            brand: "Nike"
+        },
+        {
+            name: "Programmer's TShirt 2",
+            imagePath: "/images/tshirt2.jpeg",
+            price: 999,
+            brand: "UCB"
+        },
+        {
+            name: "Programmer's TShirt 3",
+            imagePath: "/images/tshirt3.jpeg",
+            price: 720,
+            brand: "Louis Vuitton"
+        },
+        {
+            name: "Programmer's TShirt 4",
+            imagePath: "/images/tshirt4.jpeg",
+            price: 400,
+            brand: "Zara"
+        },
+        {
+            name: "Programmer's TShirt 5",
+            imagePath: "/images/tshirt5.jpeg",
+            price: 560,
+            brand: "Allen Soly"
+        },
+        {
+            name: "Programmer's TShirt 6",
+            imagePath: "/images/tshirt6.jpeg",
+            price: 420,
+            brand: "Adidas"
+        }
+    ]
+}
